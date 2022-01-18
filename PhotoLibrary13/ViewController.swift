4cc990107dc36622674a8bd8fd61c3533089dbc4
Igor Kuzmin <lamnya@gmail.com>
2022-01-18 20:45:27 +0800

    Initial Commit

---
+++ PhotoLibrary13/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoLibrary13
+//
+//  Created by Igor Kuzmin on 18.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
