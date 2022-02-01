2755c198f8e6bf91b65c3a7a36bced10abd17479
Igor Kuzmin <lamnya@gmail.com>
2022-02-01 17:40:52 +0800

    Add UICollection.

--- PhotoLibrary13/View/PhotoLibraryView.swift
+++ PhotoLibrary13/View/PhotoLibraryView.swift
@@ -13,10 +13,38 @@ class PhotoLibraryView: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        let layout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets (top: 20, left: 10, bottom: 10, right: 10)
+        layout.itemSize = CGSize(width: 60, height: 60)
+
+        let myCollectionView = UICollectionView(frame: view.frame, collectionViewLayout: layout)
+        myCollectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        myCollectionView.backgroundColor = .red
+
+        myCollectionView.dataSource = self
+
+        view.addSubview(myCollectionView)
+
         view.backgroundColor = .cyan
         navigationItem.leftBarButtonItem = addBarButton
         navigationItem.title = "Альбомы"
         navigationController?.navigationBar.prefersLargeTitles = true
+
+    }
+
+}
+
+extension PhotoLibraryView: UICollectionViewDataSource {
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 9
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let myCell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        myCell.backgroundColor = UIColor.blue
+        return myCell
     }
 
 }
